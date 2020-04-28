0cd84d6b736bd3510b57c90856ea3e10e0309366
subhamkrazy65 <shubhamk314@gmail.com>
2020-04-28 14:40:52 +0530

    Updates

---
+++ precache-manifest.7fda8aad9bc0f4404f715e346e32e95f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfee86ac6983f9224fb1ee627b7cf7f6",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "e80c95dab6e5a410066b",
+    "url": "/todolist-app/static/css/main.9c7c5a25.chunk.css"
+  },
+  {
+    "revision": "626b9f71ef3827b2da37",
+    "url": "/todolist-app/static/js/2.389e5bbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-app/static/js/2.389e5bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80c95dab6e5a410066b",
+    "url": "/todolist-app/static/js/main.8975a7d9.chunk.js"
+  },
+  {
+    "revision": "e3cdfc83d6ac76b6857e",
+    "url": "/todolist-app/static/js/runtime-main.0036258a.js"
+  }
+]);
